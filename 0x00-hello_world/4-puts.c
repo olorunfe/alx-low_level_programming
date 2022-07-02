960c6ca5247ca6193408abad537b92d18af4fff4
olorunfe <olorunfemie212@gmail.com>
2022-07-02 18:36:46 +0100

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-/*main:
- * description: print the word "\"Programming is like building a multilingual puzzle"
- * return 0
+/*
+ * main -
+ * Description: print the word "\"Programming is like building a multilingual puzzle"
+ * Return: 0
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
