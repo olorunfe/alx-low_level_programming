4f811a0034f68420c0b541920356d3a4a938b009
olorunfe <olorunfemie212@gmail.com>
2022-07-02 18:34:07 +0100

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+
+/*main:
+ * description: print the word "\"Programming is like building a multilingual puzzle"
+ * return 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
