895ba61c3c98f0b387b1ffb5ff90bcb46e90b60a
olorunfe <olorunfemie212@gmail.com>
2022-07-02 19:14:29 +0100

    Printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  * main - Entry point
  * Description: print an sentence
- * Return: (0)
+ * Return: 0
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
