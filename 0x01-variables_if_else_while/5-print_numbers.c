41d14e61e842c6c193f25bec260090525f1b1ce7
olorunfe <olorunfemie212@gmail.com>
2022-07-03 14:07:09 +0100

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry
+ * Description: yeah
+ * Return: 0
+ */
+int main(void)
+{
+	char c = '0';
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
