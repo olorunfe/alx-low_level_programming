a4375449b0c781dbe838e9278f8d09ab026b63aa
olorunfe <olorunfemie212@gmail.com>
2022-07-06 20:13:39 +0100

    Absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,16 +2,18 @@
 #include "main.h"
 /**
  * _abs - function
- *
+ * @n: quarying
  * Return: n
  */
 int _abs(int n)
 {
 	if (n >= 0)
+	{
 		return (n);
+	}
 	else
 	{
-	       	(n *= -1);
+		(n *= -1);
 		return (n);
 	}
 }
