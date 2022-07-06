3e5b31bdf24a6a6487f287122b161619534ac2cc
olorunfe <olorunfemie212@gmail.com>
2022-07-06 17:54:07 +0100

    10 rows of the alphabet!

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - entry point
+ *
+ * Description: Prints the alphabet with _putchar ten times and a newline
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int j;
+	char a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
