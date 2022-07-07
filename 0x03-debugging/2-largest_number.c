dcb4f31c974dff75e1cc34d613bdf56fdcb7f216
olorunfe <olorunfemie212@gmail.com>
2022-07-07 12:41:37 +0100

    0 > 972

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,7 +5,7 @@
  * @a: first integer
  * @b: second integer
  * @c: third integer
- * Return: largest number
+ * Return: largest
  */
 
 int largest_number(int a, int b, int c)
@@ -20,7 +20,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else if (a == b || a == c || b == c)
+	else if (a == b)
 	{
 		largest = a;
 	}
