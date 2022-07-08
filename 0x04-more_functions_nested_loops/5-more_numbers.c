4cf215e48ec74a169a9300c7555a057d0536aa6f
olorunfe <olorunfemie212@gmail.com>
2022-07-08 10:45:00 +0100

    Universal Language

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints a set of numbers 10 times
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	char c;
+	int i = 0;
+	for (c = '0'; c <= '14'; c++)
+		if (i < 10; i++)
+			_putchar(c);
+	_putchar('\n');
+}
