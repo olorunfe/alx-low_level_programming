a99eec05315de40885bc664c410262c7fd040334
olorunfe <olorunfemie212@gmail.com>
2022-07-08 07:57:48 +0100

    numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,12 +3,14 @@
 /**
  * print_numbers - print numbers 0 to 9
  * @c: variable
- * _putchar: c
+ * Return: void
  */
 
 void print_numbers(void)
 {
 	for ( c='0', c<'9', c++)
+	{
 		_putchar(c);
-	_putchar(\n);
+	}
+	_putchar('\n');
 }
