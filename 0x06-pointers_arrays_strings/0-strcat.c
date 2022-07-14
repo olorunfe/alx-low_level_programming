1c4b89aebf4cf083f6aea378ac55af254a5fefe1
olorunfe <olorunfemie212@gmail.com>
2022-07-14 23:40:34 +0100

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,13 +12,10 @@ char *_strcat(char *dest, char *src)
 	int a = -1;
 	int i;
 	for (i = 0; dest[i] != '\0'; i++);
-	do 
-	{
+	do {
 		a++;
 		dest[i] = src[a];
 		i++;
-	}
-	while (src[a] != '\0');
-
+	} while (src[a] != '\0');
 	return (dest);
 }
